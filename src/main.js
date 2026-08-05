a3909ade64a72e0a9713ac1b11296abd2dfd2523
connandgo <connandgo@gmail.com>
2026-08-05 10:26:21 +0900

    [style] Element Plus 테마를 프로젝트 무채색 디자인에 맞춤

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// Element Plus 기본 색을 프로젝트 토큰으로 덮어쓴다 (반드시 위 CSS 다음)
+import './assets/element-theme.css'
 
 const app = createApp(App)
 
